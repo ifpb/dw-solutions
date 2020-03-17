0cffa081c517cf1590060b3e502b69b2ebef9450
ernandez-ds <59183455+ernandez-ds@users.noreply.github.com>
2020-03-17 13:40:17 -0300

    tarefa isc-dhcp (#53)
    * commit 1 - Ernandez D Santos
    
    * Adicionando Exe1 - primeiraPaginaWeb
    
    * Alterações sugeridas pelo professor
    
    * Exercicios de Estilo com css
    
    * Troca de pc
    
    * curriculim-vitae
    
    * curriculim-vitae-2
    
    * primeito boot-stratp - hello World!
    
    * tarefa isc-dhcp
    
    Co-authored-by: “ernandez” <“ernandez.santos@academico.ifpb.edu.br”>
    Co-authored-by: Luiz Carlos <lucachaves@gmail.com>

--- front-end/screenshot-puppeteer.js
+++ front-end/screenshot-puppeteer.js
@@ -2,17 +2,22 @@ const util = require("util");
 let exec = require("child_process").exec;
 let exec_prom = util.promisify(exec);
 const puppeteer = require("puppeteer");
+const path = ".";
 
-// TODO Screenshot AJAX
 (async () => {
-  let htmls = await exec_prom("find . -type f -name '*.html'");
+  let htmls = await exec_prom(`find ${path} -type f -name '*.html'`);
   htmls = htmls.stdout.split("\n").filter(n => n !== "");
   const browser = await puppeteer.launch();
   for (let html of htmls) {
     html = `${__dirname}/${html}`;
     const image = `${html}.png`;
     const page = await browser.newPage();
-    await page.goto(`file://${html}`, { waitUntil: "domcontentloaded" });
+    // await page.goto(`file://${html}`, { waitUntil: "domcontentloaded" });
+    await page.goto(`file://${html}`, {
+      waitUntil: "networkidle0",
+      timeout: 60000
+    });
+    // await page.setViewport({ width: 1024, height: 800 });
     await page.setViewport({ width: 1440, height: 900 });
     await page.screenshot({ fullPage: true, path: image });
     await page.close();
