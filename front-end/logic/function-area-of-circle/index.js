0219b1f9d4b64861441674c7765d3b4f6a630478
Vinicius Accioly <vinicius.accioly@academico.ifpb.edu.br>
2020-09-25 09:12:27 -0300

    Exercicio function-area-of-circle

---
+++ front-end/logic/function-area-of-circle/index.js
@@ -0,0 +1,15 @@
+// Área do Círculo
+// A = P(r)n2
+
+
+function circle(r){
+    const p = 3.141592653589793
+    const A = p * (r**2)
+
+    console.log(A)
+}
+
+circle(1)
+circle(2)
+circle(10)
+circle(100)
