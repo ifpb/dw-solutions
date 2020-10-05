770c850ae65b58ece407462d5f1259469736a2a0
Vinicius Accioly <vinicius.accioly@academico.ifpb.edu.br>
2020-10-05 00:02:35 -0300

    Exercicio Semana4 - Objetos hosts json

--- front-end/logic/object-hosts/index.js
+++ front-end/logic/object-hosts/index.js
@@ -1,5 +1,4 @@
-let hosts = [
-	{hostname: 'PC 1', address:'192.168.0.1', mask: '255.255.255.0'}, {hostname: 'Server', address:'10.0.0.1', mask: '255.255.255.0'},
+let hosts = [{hostname: 'PC 1', address:'192.168.0.1', mask: '255.255.255.0'}, {hostname: 'Server', address:'10.0.0.1', mask: '255.255.255.0'},
 
 ];
 
