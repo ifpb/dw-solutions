89047edaa854869b808b9d0bcc310e7fa2fa3ebd
Fernando Augusto <fernandoaugusto0988@gmail.com>
2020-10-09 17:47:54 -0300

    exercicio-function-area-of-circle

--- front-end/logic/function-sum/fernando.viana/index.js
+++ front-end/logic/function-sum/fernando.viana/index.js
@@ -1,10 +1,7 @@
 function sum (v1, v2) {
     return v1 + v2;
 }
-console.log(sum(1,2));
+console.log(sum(1, 2));
 
-
-let v1 = 3;
-let v2 = 2;
-let soma = sum(v1, v2);
-console.log(soma);
+const soma = (p1, p2) => p1 + p2;
+console.log(soma(3, 2));
